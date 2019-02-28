838b1c9230c79e00cc9044d5799d23179de657c9
Austin637 <austinesparza@gmail.com>
2019-02-28 15:31:59 -0800

    footer icons

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Header.css';
-import HeaderTitle from "./HeaderTitle";
 import HeaderContent from "./HeaderContent";
 import ImageThumbnail from "./ImageThumbnail";
 
@@ -8,20 +7,10 @@ function HeaderContainer() {
 	return ( <
 		div className = "header" >
 		<
-		div >
-		<
 		ImageThumbnail / >
-		<
-		/div> <
-		div className = "header-text" >
-		<
-		HeaderTitle title = "Lambda School"
-		handle = "@LambdaSchool"
-		date = "26 Jan" / >
 		<
 		HeaderContent / >
 		<
-		/div> <
 		/div>
 	)
 }
